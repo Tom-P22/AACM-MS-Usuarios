9a1232d05e8c1589605e66c852ef6d38245642f5
TomP22 <to.penap@duocuc.cl>
2026-06-28 23:13:39 -0400

    chore(config): actualizar metadatos de documentación en SwaggerConfig

--- src/main/java/cl/municipalidad/usuarios/config/SwaggerConfig.java
+++ src/main/java/cl/municipalidad/usuarios/config/SwaggerConfig.java
@@ -1,10 +1,11 @@
 package cl.municipalidad.usuarios.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 @Configuration
 public class SwaggerConfig {
 
@@ -14,6 +15,10 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("API de Microservicio de Usuarios")
                         .description("Endpoints para la administración, validación y control de usuarios municipales")
-                        .version("1.0.0"));
-    }
+                        .version("1.0.0")
+                        .description("Servicio crítico del ecosistema distribuido encargado de la administración corporativa de cuentas, validación de identidades (RUT), perfiles y provisión de datos de autenticación internos.")
+                        .contact(new Contact()
+                                .name("Departamento de Informática Municipal")
+                                .email("soporte.ti@municipalidad.cl")));
+}
 }
